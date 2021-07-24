b70aa6bf9ecb76bb3150dd15b54b2fc9a0aad33f
elicitmind <elicitmind@gmail.com>
2021-07-24 09:45:22 +0200

    lean code practice, components organization, props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses"
+import expensesData from "./components/expensesData"
 
 function App() {
-  return (
-    <div>
-      <h2> Let 's get started!</h2>
-      <ExpenseItem />
-    </div>
-  );
+
+
+    return (
+        <div>
+            <h2> NOW WE SHALL BEGIN</h2>
+            <Expenses {...expensesData} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/expensesData.js
@@ -0,0 +1,27 @@
+const expensesData = [{
+        id: 'e1',
+        title: 'Vegetables',
+        amount: 24.12,
+        date: new Date(2021, 6, 14),
+    },
+    {
+        id: 'e2',
+        title: 'Courses',
+        amount: 199.49,
+        date: new Date(2021, 6, 12),
+    },
+    {
+        id: 'e3',
+        title: 'Nuts & Quinoa',
+        amount: 34.67,
+        date: new Date(2021, 5, 28),
+    },
+    {
+        id: 'e4',
+        title: 'Hardware',
+        amount: 450,
+        date: new Date(2021, 6, 12),
+    },
+];
+
+export default expensesData
